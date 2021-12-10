7ff1f8c27a9cd20f2387c5dc7ddc82860adc6f51
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2021-12-10 23:59:44 +0300

    Starting with task 2

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "christmas-challenge/day_01"
 
 func main(){
 	rawDataFilename := "elevation.txt"
-	day_01.GetRawDataFromWeb(rawDataFilename)
 	elevationCount := day_01.CountElevations(rawDataFilename)
 	println(elevationCount)
 
