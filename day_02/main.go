3cadeba490a15b911c14363c25b912ff2d8dc87a
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2021-12-13 22:53:34 +0300

    First day 3 attempt

--- day_02/main.go
+++ day_02/main.go
@@ -1,7 +1,48 @@
 package day_02
 
-import "fmt"
+import (
+	"bufio"
+	"christmas-challenge/helpers"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Print("blah")
+	rawDataFilename := "directions.txt"
+	helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/2/input")
+	horPosition := 0
+	depth := 0
+	aim := 0
+
+	rawDataFile, err := os.Open(rawDataFilename)
+	if nil == err {
+		scanner := bufio.NewScanner(rawDataFile)
+		for scanner.Scan() {
+			currentText := scanner.Text()
+			textSlices := strings.Split(currentText, " ")
+			if len(textSlices) == 2 {
+				direction := textSlices[0]
+				value, conversionError := strconv.Atoi(textSlices[1])
+				if conversionError == nil{
+					switch direction{
+					case "down":
+						aim += value
+					case "up":
+						aim -= value
+					case "forward":
+						horPosition += value
+						depth += aim*value
+					default:
+						println("incorrect direction " + direction)
+					}
+
+				}
+
+			}
+
+		}
+	}
+	fmt.Printf("Depth %d hor position %d answer to enter %d", depth, horPosition, depth*horPosition)
 }
--- main.go
+++ main.go
@@ -2,45 +2,56 @@ package main
 
 import (
 	"bufio"
-	"christmas-challenge/helpers"
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
-	rawDataFilename := "directions.txt"
-	helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/2/input")
-	horPosition := 0
-	depth := 0
+	rawDataFilename := "gamma_epsion.txt"
+	// helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/3/input")
+	const exampleString = "101011011110"
+	expected_string_length := len(exampleString)
+	number_count := 0
+	gamma_one_frequency := make([]int, expected_string_length)
 
 	rawDataFile, err := os.Open(rawDataFilename)
 	if nil == err {
 		scanner := bufio.NewScanner(rawDataFile)
 		for scanner.Scan() {
 			currentText := scanner.Text()
-			textSlices := strings.Split(currentText, " ")
-			if len(textSlices) == 2 {
-				direction := textSlices[0]
-				value, conversionError := strconv.Atoi(textSlices[1])
-				if conversionError == nil{
-					switch direction{
-					case "down":
-						depth += value
-					case "up":
-						depth -= value
-					case "forward":
-						horPosition += value
-					default:
-						println("incorrect direction " + direction)
-					}
-
+			if len(currentText) == 0 {continue}
+			if len(currentText) != expected_string_length {
+				fmt.Errorf("string '%s' is not valid expected something like '%s'", currentText, exampleString)
+			}
+			number_count++
+			for i := 0; i < expected_string_length; i++{
+				if currentText[i] == '1'{
+					gamma_one_frequency[i]++
+				} else if currentText[i] != '0'{
+					panic(fmt.Errorf("unexpected symbol %s in string %s", string(currentText[i]), currentText))
 				}
-
 			}
 
 		}
 	}
-	fmt.Printf("Depth %d hor position %d answer to enter %d", depth, horPosition, depth*horPosition)
+	println(gamma_one_frequency)
+	gamma := uint16(0)
+	epsilon := uint16(0)
+	for i, frequency := range gamma_one_frequency{
+		if frequency > (number_count / 2){
+			gamma = gamma | (0x0001 << i)
+		} else{
+			epsilon = epsilon | (0x0001 << i)
+		}
+	}
+
+	fmt.Println(strconv.FormatInt(int64(gamma), 2))
+	fmt.Println(strconv.FormatInt(int64(epsilon), 2))
+
+	println(gamma*epsilon)
+
 }
+// 101111010000
+// 010000101111
+
