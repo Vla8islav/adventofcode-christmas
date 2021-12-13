fe81497288d66a9b4039df9c4af14f522d4a5229
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2021-12-13 23:25:15 +0300

    Solved the example for day 3

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 func main() {
-	rawDataFilename := "gamma_epsion.txt"
+	rawDataFilename := "gamma_epsion_sample.txt"
 	// helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/3/input")
-	const exampleString = "101011011110"
+	// const exampleString = "101011011110"
+	const exampleString = "11110"
 	expected_string_length := len(exampleString)
 	number_count := 0
 	gamma_one_frequency := make([]int, expected_string_length)
@@ -32,26 +33,33 @@ func main() {
 					panic(fmt.Errorf("unexpected symbol %s in string %s", string(currentText[i]), currentText))
 				}
 			}
-
 		}
 	}
+
 	println(gamma_one_frequency)
 	gamma := uint16(0)
 	epsilon := uint16(0)
+	leftShift := expected_string_length - 1
 	for i, frequency := range gamma_one_frequency{
 		if frequency > (number_count / 2){
-			gamma = gamma | (0x0001 << i)
+			gamma = gamma | ((0x0001 << leftShift) >> i)
 		} else{
-			epsilon = epsilon | (0x0001 << i)
+			epsilon = epsilon | ((0x0001 << leftShift) >> i)
 		}
 	}
 
-	fmt.Println(strconv.FormatInt(int64(gamma), 2))
-	fmt.Println(strconv.FormatInt(int64(epsilon), 2))
+	fmt.Printf("%05s\n", strconv.FormatInt(int64(gamma), 2))
+	fmt.Printf("%05s\n", strconv.FormatInt(int64(epsilon), 2))
+	fmt.Printf("gamma %d epsilon %d", gamma, epsilon)
 
 	println(gamma*epsilon)
 
 }
-// 101111010000
-// 010000101111
+
+// 01101
+// 10010
+
+
+//101111010000
+//010000101111
 
