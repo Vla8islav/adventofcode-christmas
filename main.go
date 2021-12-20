5837e564a780864d7d12f433a0aa244cf5937843
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2021-12-20 23:00:59 +0300

    Works, but the wrong way

--- main.go
+++ main.go
@@ -4,62 +4,84 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
+
+var exampleString = "110100101110"
+var expected_string_length = len(exampleString)
+
+func calculateFrequency(numbersArray []string) []int {
+	onesFrequency := make([]int, expected_string_length)
+	for _, binaryNumberString := range numbersArray {
+		for i, letter := range binaryNumberString {
+			if letter == '1' {
+				onesFrequency[i]++
+			}
+		}
+	}
+	return onesFrequency
+}
+
+func calculateTendency(numbersArray []string) []int {
+	freqArray := calculateFrequency(numbersArray)
+	tendency := make([]int, expected_string_length)
+	for i, frequency := range freqArray {
+		if frequency > len(numbersArray)/2 {
+			tendency[i] = 1
+		}
+	}
+	return tendency
+}
 
 func main() {
 	rawDataFilename := "gamma_epsion.txt"
 	// helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/3/input")
 	// const exampleString = "101011011110"
-	const exampleString = "110100101110"
-	expected_string_length := len(exampleString)
-	number_count := 0
-	gamma_one_frequency := make([]int, expected_string_length)
+	numbersArray := extractBinaryNumbers(rawDataFilename, exampleString)
+	numbersArrayLen := len(numbersArray)
+	numbersTendency := calculateTendency(numbersArray)
 
-	rawDataFile, err := os.Open(rawDataFilename)
-	if nil == err {
-		scanner := bufio.NewScanner(rawDataFile)
-		for scanner.Scan() {
-			currentText := scanner.Text()
-			if len(currentText) == 0 {continue}
-			if len(currentText) != expected_string_length {
-				fmt.Errorf("string '%s' is not valid expected something like '%s'", currentText, exampleString)
-			}
-			number_count++
-			for i := 0; i < expected_string_length; i++{
-				if currentText[i] == '1'{
-					gamma_one_frequency[i]++
-				} else if currentText[i] != '0'{
-					panic(fmt.Errorf("unexpected symbol %s in string %s", string(currentText[i]), currentText))
+	oxygenFlagArray := make([]bool, numbersArrayLen)
+	for i, _ := range oxygenFlagArray {
+		oxygenFlagArray[i] = true
+	}
+
+	oxygenRemainingNumberCount := len(numbersArray)
+
+	for position, _ := range numbersArray[0] {
+		for i, binaryNumberString := range numbersArray {
+			if oxygenFlagArray[i] {
+				if !(binaryNumberString[position] == '1' && numbersTendency[position] == 1) {
+					oxygenFlagArray[i] = false
+					oxygenRemainingNumberCount--
 				}
 			}
 		}
 	}
 
-	println(gamma_one_frequency)
-	gamma := uint16(0)
-	epsilon := uint16(0)
-	leftShift := expected_string_length - 1
-	for i, frequency := range gamma_one_frequency{
-		if frequency > (number_count / 2){
-			gamma = gamma | ((0x0001 << leftShift) >> i)
-		} else{
-			epsilon = epsilon | ((0x0001 << leftShift) >> i)
+}
+
+func extractBinaryNumbers(rawDataFilename string, exampleString string) []string {
+	arrayOfValues := make([]string, 0)
+	expected_string_length := len(exampleString)
+	rawDataFile, err := os.Open(rawDataFilename)
+	if nil == err {
+		scanner := bufio.NewScanner(rawDataFile)
+		for scanner.Scan() {
+			currentText := scanner.Text()
+			if len(currentText) == 0 {
+				continue
+			}
+			if len(currentText) != expected_string_length {
+				fmt.Errorf("string '%s' is not valid expected something like '%s'", currentText, exampleString)
+			}
+			arrayOfValues = append(arrayOfValues, currentText)
 		}
 	}
-
-	fmt.Printf("%012s\n", strconv.FormatInt(int64(gamma), 2))
-	fmt.Printf("%012s\n", strconv.FormatInt(int64(epsilon), 2))
-	fmt.Printf("gamma %d epsilon %d", gamma, epsilon)
-
-	println(int64(gamma)*int64(epsilon))
-
+	return arrayOfValues
 }
 
 // 01101
 // 10010
 
-
 //101111010000
 //010000101111
-
