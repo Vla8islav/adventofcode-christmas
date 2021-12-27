36853147f16c0cf61645fdbc8048c7389b6e8ee9
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2021-12-27 22:13:07 +0300

    Started working on day 05

---
+++ day_05/main.go
@@ -0,0 +1,9 @@
+package day_05
+
+import "christmas-challenge/helpers"
+
+func RunDay05() {
+	rawDataFilename := "day_05.txt"
+	helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/5/input")
+
+}
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"christmas-challenge/day_04"
+	"christmas-challenge/day_05"
 )
 
 
 func main() {
 	// helpers.GetRawDataFromWeb(rawDataFilename, "https://adventofcode.com/2021/day/3/input")
 	// const exampleString = "101011011110"
-	day_04.RunDay04()
+	day_05.RunDay05()
 
 }
